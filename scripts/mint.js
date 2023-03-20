0fb043d1551c10de0cd6d83c72d923979841f84e
MonsteriousCOD <93883450+MonsteriousCOD@users.noreply.github.com>
2023-03-20 13:32:07 +0100

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,16 +1,671 @@
-/*
 let blockchain = {};
 let contractAddress = "0xb018b17aE9DCA2b684cd05CD07a81bFf8Cc17E8C";
 let mintingPrice = 0.01;
+
+// Add the ABI array here
+const abi = [[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "ApprovalCallerNotOwnerNorApproved",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "ApprovalQueryForNonexistentToken",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "BalanceQueryForZeroAddress",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "flipSale",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "MintERC2309QuantityExceedsLimit",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "MintToZeroAddress",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "quantity",
+				"type": "uint256"
+			}
+		],
+		"name": "mintVikings",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "MintZeroQuantity",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "OwnerQueryForNonexistentToken",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "OwnershipNotInitializedForExtraData",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "TransferCallerNotOwnerNorApproved",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "TransferFromIncorrectOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "TransferToNonERC721ReceiverImplementer",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "TransferToZeroAddress",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "URIQueryForNonexistentToken",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "approved",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "fromTokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "toTokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "ConsecutiveTransfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "uri",
+				"type": "string"
+			}
+		],
+		"name": "setBaseURI",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_newPrice",
+				"type": "uint256"
+			}
+		],
+		"name": "setPrice",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "teamMint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "baseURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "getApproved",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxPerTx",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxPerWallet",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxVikings",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "mintEnabled",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "publicPrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "teamReserved",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+  
+];
+
 function isEthAddress() {
   return (
     !!blockchain.account && /^(0x)?[0-9a-f]{40}$/i.test(blockchain.account)
   );
 }
+
 function setAddress(addr){
   contractAddress=addr;
   console.log("Address set to " + contractAddress);
 }
+
 async function connect(callback) {
   
   if (window.ethereum === undefined) {
@@ -19,6 +674,7 @@ async function connect(callback) {
     console.warn('no eth installed');
     return false;
   }
+  
   if (!isEthAddress()) {
     console.log("WEB3 DETECT")
     const isEnabled = await window.ethereum.enable();
@@ -29,90 +685,91 @@ async function connect(callback) {
     }
   }
  
-  console.log("Contracct is: " + contractAddress.length);
+  console.log("Contract is: " + contractAddress.length);
+  
   if (isEthAddress()) {
     console.log("WEB3 FINE")
     const web3 = new Web3(window.ethereum);
-    const myContract = new web3.eth.Contract(abiContract.abi,contractAddress);
+    // Replace abiContract with abi here
+    const myContract = new web3.eth.Contract(abi, contractAddress);
     blockchain = {account: blockchain.account,myContract,web3,};
   }
+  
   callback && callback(isEthAddress());
   return isEthAddress();
 }
+
 async function mintVik(quantity, successContainer) {
-  if (!(await connect())) {
-    console.log('No Connection to eth');
-    return;
+    if (!(await connect())) {
+      console.log('No Connection to eth');
+      return;
+    }
+    
+    const publicPrice = 0.01;
+    const normalPrice = publicPrice * quantity;
+    try {
+      let gas = await blockchain.myContract.methods
+        .mintVikings(quantity)
+        .estimateGas({
+          from: blockchain.account,
+          value: normalPrice,
+        });
+      console.log("ESTIMATED GAS0: " + gas);
+      gas *= 1.2;
+      gas = Math.round(gas);
+      blockchain.myContract.methods
+        .mintVikings(quantity)
+        .send({
+          from: blockchain.account,
+          value: normalPrice,
+          gas: gas,
+          maxFeePerGas: 2500000001, // 1.5 Gwei
+          maxPriorityFeePerGas: 2000000000, // .5 Gwei
+          type: '0x2'
+        })
+        .once('error', (err) => {
+          console.log(err);
+        })
+        .then((receipt) => {
+          console.log(receipt);
+          successContainer.style.visibility = 'visible';
+        });
+    } catch (error) {
+      console.log(error);
+    }
   }
-  
-  const publicPrice = 0.01;
-  const normalPrice = publicPrice * quantity;
-
-  try {
-    let gas = await blockchain.myContract.methods
-      .mintVikings(quantity)
-      .estimateGas({
-        from: blockchain.account,
-        value: normalPrice,
-      });
-    console.log("ESTIMATED GAS0: " + gas);
-    gas *= 1.2;
-    gas = Math.round(gas);
-
-    blockchain.myContract.methods
-      .mintVikings(quantity)
-      .send({
-        from: blockchain.account,
-        value: normalPrice,
-        gas: gas,
-        maxFeePerGas: 2500000001, // 1.5 Gwei
-        maxPriorityFeePerGas: 2000000000, // .5 Gwei
-        type: '0x2'
-      })
-      .once('error', (err) => {
-        console.log(err);
-      })
+  async function getPublicPrice(callback) {
+    let price = 0;
+    await blockchain.myContract.methods
+      ._publicPrice()
+      .call()
       .then((receipt) => {
-        console.log(receipt);
-        successContainer.style.visibility = 'visible';
+        console.log("Public price is: " + receipt);
+        price = receipt;
+        callback && callback(price);
+        return price;
       });
-  } catch (error) {
-    console.log(error);
+    callback && callback(price);
+    return price;
   }
-}
-async function getPublicPrice(callback) {
-  let price = 0;
-  await blockchain.myContract.methods
-    ._publicPrice()
-    .call()
-    .then((receipt) => {
-      console.log("Public price is: " + receipt);
-      price = receipt;
-      callback && callback(price);
-      return price;
-    });
-  callback && callback(price);
-  return price;
-}
-async function getTotalSupply(callback) {
-  let supply = 0;
-  await blockchain.myContract.methods
-    .totalSupply()
-    .call()
-    .then((receipt) => {
-      console.log("Supply is: " + receipt);
-      supply = receipt;
-      callback && callback(supply);
-      return supply;
-    });
-  callback && callback(supply);
-  return supply;
-}
-//series page - range
-$('.rangeInput').on('input', (e) => {
-  $('.rangeValue').text(e.currentTarget.value);
-});
-$('.rangeInput').on('input', (e) => {
-  $('.rangePrice').text((Math.round(e.currentTarget.value * 0.01 * 10000) / 10000).toString() + " ETH");
-});
-*/
+  async function getTotalSupply(callback) {
+    let supply = 0;
+    await blockchain.myContract.methods
+      .totalSupply()
+      .call()
+      .then((receipt) => {
+        console.log("Supply is: " + receipt);
+        supply = receipt;
+        callback && callback(supply);
+        return supply;
+      });
+    callback && callback(supply);
+    return supply;
+  }
+  //series page - range
+  $('.rangeInput').on('input', (e) => {
+    $('.rangeValue').text(e.currentTarget.value);
+  });
+  $('.rangeInput').on('input', (e) => {
+    $('.rangePrice').text((Math.round(e.currentTarget.value * 0.01 * 10000) / 10000).toString() + " ETH");
+  });
